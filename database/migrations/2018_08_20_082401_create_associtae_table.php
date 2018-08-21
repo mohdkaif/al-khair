ccf7152305f978bb618b362ca2ed25cba48c82d2
rupesh <rupesh@singsys.com>
2018-08-21 12:36:28 +0530

    cahnges in migration file

--- database/migrations/2018_08_20_082401_create_associtae_table.php
+++ database/migrations/2018_08_20_082401_create_associtae_table.php
@@ -13,7 +13,7 @@ class CreateAssocitaeTable extends Migration
      */
     public function up()
     {
-        Schema::create('associate', function (Blueprint $table) {
+        Schema::create('associates', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('user_id');
