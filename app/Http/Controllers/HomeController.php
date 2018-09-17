f4cf36c5673325c2b4c3ba83e9694c00f64a4c77
sheenoy <shoaib2fly@gmail.com>
2018-09-17 17:59:25 +0530

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct(Request $request)
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
         parent::__construct($request);
     }
 
